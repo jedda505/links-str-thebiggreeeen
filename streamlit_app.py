4fc69941eb6dd7e615e94b32a22791fabbadccc0
jedda505 <jdutton.505@gmail.com>
2022-10-27 18:19:51 +0100

    testing scheduler again

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,10 @@ st_button('discord', 'https://discord.gg/xs83PBTcw2', 'Our Discord Community', i
 
 
 def od_job():
-    schedule.every().thursday.at("18:05").do(
-        st_button('open_deck', 'https://docs.google.com/spreadsheets/d/17nO20xzAFPw7ulF7iJgfqj7Lem-u_yBB5ljWiwVBBsQ/edit#gid=0Q/edit#gid=0', 'PALMS DJ JAM OPEN DECKS SIGN UP - 5th October 2022', icon_size)
+    schedule.every().thursday.at("18:23").do(
+        While True:
+            st_button('open_deck', 'https://docs.google.com/spreadsheets/d/17nO20xzAFPw7ulF7iJgfqj7Lem-u_yBB5ljWiwVBBsQ/edit#gid=0Q/edit#gid=0', 'PALMS DJ JAM OPEN DECKS SIGN UP - 5th October 2022', icon_size)
     )
-    return schedule.CancelJob
 
 while True:
     schedule.run_pending()
